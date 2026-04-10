512181610a5c49ec9d9fdd6f067c8ffa25c4ac87
thototh <144813512+thototh@users.noreply.github.com>
2026-04-10 11:30:35 +0900

    Update 1-3.c

---
+++ code/1-3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main(){
+    char *name[5] = {"kim", "lee", "park", "choi", "jeong"};
+
+    for(int i = 0; i < 5; i++){
+        for(int j = 0; ; j++){
+            char c = *(*(name + i)+j);
+            if(c=='\0') break;
+            printf("%c", c);
+        }
+        printf("\n");
+    }
+
+
+    return 0;
+}
